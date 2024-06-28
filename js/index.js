2b71bcfd8d51f45f17ec6ba94855f4cc4886f9df
Ramin2009Mc <118001668+Ramin2009Mc@users.noreply.github.com>
2024-06-28 21:12:32 +0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,302 @@
+let placeNode = document.querySelector(`.places`);
+let x1y2 = placeNode.querySelector(`#x1y2`);
+let placeCountNode = document.querySelector(`#place-count`);
+let priceNode = document.querySelector(`#price`);
+let ThankNode = document.querySelector(`.Thank`);
+let confNode = document.querySelector(`.conf`);
+let placesCounter = 0;
+let price = 0;
+let counter = 0;
+
+
+
+x1y2.addEventListener(`click`, function () {
+    counter += 1;
+    if (counter % 2 != 0){
+        x1y2.classList.add(`active`);
+        placesCounter += 1;
+        price += 500;
+        placeCountNode.innerHTML = placesCounter;
+        priceNode.innerHTML = price;
+    }
+    else {
+        x1y2.classList.remove(`active`);
+        placesCounter -= 1;
+        price -= 500;
+        placeCountNode.innerHTML = placesCounter;
+        priceNode.innerHTML = price;
+    }
+})
+
+let counter2 = 0;
+x1y3.addEventListener(`click`, function () {
+    counter2 += 1;
+    if (counter2 % 2 != 0){
+        x1y3.classList.add(`active`);
+        placesCounter += 1;
+        price += 500;
+        placeCountNode.innerHTML = placesCounter;
+        priceNode.innerHTML = price;
+    }
+    else {
+        x1y3.classList.remove(`active`);
+        placesCounter -= 1;
+        price -= 500;
+        placeCountNode.innerHTML = placesCounter;
+        priceNode.innerHTML = price;
+    }
+})
+
+let counter3 = 0;
+x1y4.addEventListener(`click`, function () {
+    counter3 += 1;
+    if (counter3 % 2 != 0){
+        x1y4.classList.add(`active`);
+        placesCounter += 1;
+        price += 500;
+        placeCountNode.innerHTML = placesCounter;
+        priceNode.innerHTML = price;
+    }
+    else {
+        x1y4.classList.remove(`active`);
+        placesCounter -= 1;
+        price -= 500;
+        placeCountNode.innerHTML = placesCounter;
+        priceNode.innerHTML = price;
+    }
+})
+
+let counter4 = 0;
+x2y1.addEventListener(`click`, function () {
+    counter4 += 1;
+    if (counter4 % 2 != 0){
+        x2y1.classList.add(`active`);
+        placesCounter += 1;
+        price += 500;
+        placeCountNode.innerHTML = placesCounter;
+        priceNode.innerHTML = price;
+    }
+    else {
+        x2y1.classList.remove(`active`);
+        placesCounter -= 1;
+        price -= 500;
+        placeCountNode.innerHTML = placesCounter;
+        priceNode.innerHTML = price;
+    }
+})
+
+let counter5 = 0;
+x2y2.addEventListener(`click`, function () {
+    counter5 += 1;
+    if (counter5 % 2 != 0){
+        x2y2.classList.add(`active`);
+        placesCounter += 1;
+        price += 500;
+        placeCountNode.innerHTML = placesCounter;
+        priceNode.innerHTML = price;
+    }
+    else {
+        x2y2.classList.remove(`active`);
+        placesCounter -= 1;
+        price -= 500;
+        placeCountNode.innerHTML = placesCounter;
+        priceNode.innerHTML = price;
+    }
+})
+
+let counter6 = 0;
+x2y3.addEventListener(`click`, function () {
+    counter6 += 1;
+    if (counter6 % 2 != 0){
+        x2y3.classList.add(`active`);
+        placesCounter += 1;
+        price += 500;
+        placeCountNode.innerHTML = placesCounter;
+        priceNode.innerHTML = price;
+    }
+    else {
+        x2y3.classList.remove(`active`);
+        placesCounter -= 1;
+        price -= 500;
+        placeCountNode.innerHTML = placesCounter;
+        priceNode.innerHTML = price;
+    }
+})
+
+let counter7 = 0;
+x2y4.addEventListener(`click`, function () {
+    counter7 += 1;
+    if (counter7 % 2 != 0){
+        x2y4.classList.add(`active`);
+        placesCounter += 1;
+        price += 500;
+        placeCountNode.innerHTML = placesCounter;
+        priceNode.innerHTML = price;
+    }
+    else {
+        x2y4.classList.remove(`active`);
+        placesCounter -= 1;
+        price -= 500;
+        placeCountNode.innerHTML = placesCounter;
+        priceNode.innerHTML = price;
+    }
+})
+
+let counter8 = 0;
+x3y1.addEventListener(`click`, function () {
+    counter8 += 1;
+    if (counter8 % 2 != 0){
+        x3y1.classList.add(`active`);
+        placesCounter += 1;
+        price += 500;
+        placeCountNode.innerHTML = placesCounter;
+        priceNode.innerHTML = price;
+    }
+    else {
+        x3y1.classList.remove(`active`);
+        placesCounter -= 1;
+        price -= 500;
+        placeCountNode.innerHTML = placesCounter;
+        priceNode.innerHTML = price;
+    }
+})
+
+let counter9 = 0;
+x3y2.addEventListener(`click`, function () {
+    counter9 += 1;
+    if (counter9 % 2 != 0){
+        x3y2.classList.add(`active`);
+        placesCounter += 1;
+        price += 500;
+        placeCountNode.innerHTML = placesCounter;
+        priceNode.innerHTML = price;
+    }
+    else {
+        x3y2.classList.remove(`active`);
+        placesCounter -= 1;
+        price -= 500;
+        placeCountNode.innerHTML = placesCounter;
+        priceNode.innerHTML = price;
+    }
+})
+
+let counter10 = 0;
+x3y3.addEventListener(`click`, function () {
+    counter10 += 1;
+    if (counter10 % 2 != 0){
+        x3y3.classList.add(`active`);
+        placesCounter += 1;
+        price += 500;
+        placeCountNode.innerHTML = placesCounter;
+        priceNode.innerHTML = price;
+    }
+    else {
+        x3y3.classList.remove(`active`);
+        placesCounter -= 1;
+        price -= 500;
+        placeCountNode.innerHTML = placesCounter;
+        priceNode.innerHTML = price;
+    }
+})
+
+let counter11 = 0;
+x3y4.addEventListener(`click`, function () {
+    counter11 += 1;
+    if (counter11 % 2 != 0){
+        x3y4.classList.add(`active`);
+        placesCounter += 1;
+        price += 500;
+        placeCountNode.innerHTML = placesCounter;
+        priceNode.innerHTML = price;
+    }
+    else {
+        x3y4.classList.remove(`active`);
+        placesCounter -= 1;
+        price -= 500;
+        placeCountNode.innerHTML = placesCounter;
+        priceNode.innerHTML = price;
+    }
+})
+
+let counter12 = 0;
+x4y1.addEventListener(`click`, function () {
+    counter12 += 1;
+    if (counter12 % 2 != 0){
+        x4y1.classList.add(`active`);
+        placesCounter += 1;
+        price += 500;
+        placeCountNode.innerHTML = placesCounter;
+        priceNode.innerHTML = price;
+    }
+    else {
+        x4y1.classList.remove(`active`);
+        placesCounter -= 1;
+        price -= 500;
+        placeCountNode.innerHTML = placesCounter;
+        priceNode.innerHTML = price;
+    }
+})
+
+let counter13 = 0;
+x4y2.addEventListener(`click`, function () {
+    counter13 += 1;
+    if (counter13 % 2 != 0){
+        x4y2.classList.add(`active`);
+        placesCounter += 1;
+        price += 500;
+        placeCountNode.innerHTML = placesCounter;
+        priceNode.innerHTML = price;
+    }
+    else {
+        x4y2.classList.remove(`active`);
+        placesCounter -= 1;
+        price -= 500;
+        placeCountNode.innerHTML = placesCounter;
+        priceNode.innerHTML = price;
+    }
+})
+
+let counter14 = 0;
+x4y3.addEventListener(`click`, function () {
+    counter14 += 1;
+    if (counter14 % 2 != 0){
+        x4y3.classList.add(`active`);
+        placesCounter += 1;
+        price += 500;
+        placeCountNode.innerHTML = placesCounter;
+        priceNode.innerHTML = price;
+    }
+    else {
+        x4y3.classList.remove(`active`);
+        placesCounter -= 1;
+        price -= 500;
+        placeCountNode.innerHTML = placesCounter;
+        priceNode.innerHTML = price;
+    }
+})
+
+let counterFinal = 0;
+x4y4.addEventListener(`click`, function () {
+    counterFinal += 1;
+    if (counterFinal % 2 != 0){
+        x4y4.classList.add(`active`);
+        placesCounter += 1;
+        price += 500;
+        placeCountNode.innerHTML = placesCounter;
+        priceNode.innerHTML = price;
+    }
+    else {
+        x4y4.classList.remove(`active`);
+        placesCounter -= 1;
+        price -= 500;
+        placeCountNode.innerHTML = placesCounter;
+        priceNode.innerHTML = price;
+    }
+})
+
+confNode.addEventListener(`click`, function () {
+    if (placesCounter > 0){
+        alert("Чек: -" + price + "₽" + ", Спасибо за покупку и" + " приятного просмотра!");
+    }
+    
+})
